ae3d198e25d7fc4fc05016088014df1a0a9b0d27
HERO <154802270+leshkaalb@users.noreply.github.com>
2024-12-24 13:56:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+};
